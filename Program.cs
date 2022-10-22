1a2bea99a6188423fb9b5d83b66fec080ba596f8
Curtis Clement <curtis.clement@wonderkind.com>
2022-10-22 23:06:28 +0200

    add json test data file

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ namespace PackageHandlerApp
     static void Main(string[] args) 
     {
       Console.WriteLine("Hello, World!");
+
+      // Import json files containing list of packages and store in variable
+
+      // Check each package for weight and then insurance, if value is under then pass to department, if over pass to insurance
+
+      // Loop through insurance to validate the insurance of each pending package (some sort of validation logic here?)
+
+      // Once all insurance cases are complete, verify the amount of packages going to each department
+
+      // Once verified, write the info for each package to a file and close the app
     }
   }
 }
