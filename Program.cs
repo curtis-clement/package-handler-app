200a311e32bcda90ae102ed0097c8333b0e33eb7
Curtis Clement <curtis.clement@wonderkind.com>
2022-10-23 15:23:57 +0200

    update department funcitonality to add a package

--- Program.cs
+++ Program.cs
@@ -26,6 +26,11 @@ namespace PackageHandlerApp
     public string name = "";
     public double maxHandledWeight { get; set; }
     public double valueRequiredForInsurnace { get; set; }
+    public List<Package> DepartmentPackages = new List<Package>();
+    public void addPackageToDepartment(Package package)
+    {
+      this.DepartmentPackages.Add(package);
+    }
     
   }
   class Program 
