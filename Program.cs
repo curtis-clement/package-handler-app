86c930f30fe2d9d3c1ab27712baced5ef652c9c0
Curtis Clement <curtis.clement@wonderkind.com>
2022-10-25 16:20:28 +0200

    refactor code into all services and separate concerns, solved dependency issue by putting services outside of main method

--- Program.cs
+++ Program.cs
@@ -4,69 +4,51 @@ using System.Collections.Generic;
 
 namespace PackageHandlerApp 
 {
-  class Program 
+  class Program
   {
+    static DepartmentService departmentService = new DepartmentService();
+    static PackageService packageService = new PackageService();
+    static InsuranceService insuranceService = new InsuranceService();
+    static void addPackage()
+    {
+      var package = packageService.createPackage();
+      if (insuranceService.performInsuranceCheck(package))
+      {
+        Console.WriteLine("Your package requires insurance and processing is momentarily suspended.");
+      }
+      else {
+        departmentService.addPackageToDepartment(package);
+      }
+    }
+
+    static void sortListOfProcessedPackagesByDepartment(List<Package> packages)
+    {
+      foreach (Package package in packages)
+      {
+        departmentService.addPackageToDepartment(package);
+      }
+    }
     static void Main(string[] args)
     {
       Console.WriteLine("Hello! Welcome to the our package handling app!");
       bool closeApp = false;
-      var departmentService = new DepartmentService();
-      var AvailableDepartments = departmentService.AvailableDepartments;
-      List<Package> HoldListForInsurance = new List<Package>();
 
       do
       {
-        var systemUser = "Curtis";
         Console.WriteLine("What would you like to do? You may choose, 'Add Package', 'Process Insurance Holds', 'Quit'");
         var selectedOption = Console.ReadLine();
 
         switch (selectedOption)
         {
           case "Add Package":
-            int id = departmentService.totalPackageCount();;
-            Console.WriteLine("Please enter a weight...");
-            var weight = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter a value...");
-            var value = Convert.ToDouble(Console.ReadLine());
-            var packageToAdd = new Package(id, weight, value);
-            if (packageToAdd.insuranceIsRequired())
-            {
-              HoldListForInsurance.Add(packageToAdd);
-              Console.WriteLine("Your package requires insurance and processing is momentarily suspended.");
-            }
-            else {
-              departmentService.addPackageToDepartment(packageToAdd);
-            }
+            addPackage();
             break;
           case "Process Insurance Holds":
-            List<Package> insuranceHoldsProcessed = new List<Package>();
-            foreach (Package packageOnHold in HoldListForInsurance)
-            {
-              Console.WriteLine
-              (
-                $"Package with id: {packageOnHold.id}, and weight: {packageOnHold.weight} requires insurance. Please verify your user identity to confirm adding insurance."
-              );
-              string userNameInput = Console.ReadLine()!;
-              if (userNameInput == systemUser)
-              {
-                departmentService.addPackageToDepartment(packageOnHold);
-                insuranceHoldsProcessed.Add(packageOnHold);
-                Console.WriteLine("Package has been processed!");
-              }
-              else
-              {
-                Console.WriteLine("Sorry, that user name did not match!");
-                break;
-              }
-            }
-
-            foreach (Package item in insuranceHoldsProcessed)
-            {
-              HoldListForInsurance.Remove(item);
-            }
+            var processedPackages = insuranceService.processPackgesHeldForInsurance();
+            sortListOfProcessedPackagesByDepartment(processedPackages);
             break;
           case "Quit":
-            if (HoldListForInsurance.Count > 0)
+            if (insuranceService.hasPackagesToProcess())
             {
               Console.WriteLine("Please proccess your insurance holds before quitting!");
               break;
--- Services/InsuranceService.cs
+++ Services/InsuranceService.cs
@@ -5,6 +5,65 @@ namespace PackageHandlerApp
 {
   public class InsuranceService
   {
-    
+    private string insuranceDepartmentEmployee = "Curtis";
+    List<Package> _packagesHeldForInsuranceCheck = new List<Package>();
+    List<Package> processedInsurancePackages = new List<Package>();
+
+    const int InsuranceThreshold = 1000;
+    private bool insuranceIsRequired(double packageValue)
+    {
+      return packageValue > InsuranceThreshold;
+    }
+
+    public bool performInsuranceCheck(Package package)
+    {
+      if (insuranceIsRequired(package.value))
+      {
+        _packagesHeldForInsuranceCheck.Add(package);
+        return true;
+      } 
+      else
+      {
+        return false;
+      }
+    }
+
+    public bool hasPackagesToProcess()
+    {
+      return _packagesHeldForInsuranceCheck.Count > 0;
+    }
+
+    public void clearCheckedInsurancePackage(List<Package> packagesToRemove)
+    {
+      foreach (Package package in packagesToRemove)
+      {
+        _packagesHeldForInsuranceCheck.Remove(package);
+      }
+    }
+    public List<Package> processPackgesHeldForInsurance()
+    {
+      List<Package> processedPackages = new List<Package>();
+      foreach (Package packageToProcess in _packagesHeldForInsuranceCheck)
+      {
+        Console.WriteLine
+        (
+          $"Package with id: {packageToProcess.id}, and weight: {packageToProcess.weight} requires insurance. Please verify your user identity to confirm adding insurance."
+        );
+        string userNameInput = Console.ReadLine()!;
+        if (userNameInput == insuranceDepartmentEmployee)
+        {
+          processedPackages.Add(packageToProcess);
+          Console.WriteLine("Insurance on this package has been verified!");
+        }
+        else
+        {
+          Console.WriteLine("Sorry, that user name did not match!");
+          break;
+        }
+      }
+      clearCheckedInsurancePackage(processedPackages);
+
+      return processedPackages;
+    }
   }
 }
